63b178320eb4c3f4d610723ee87aa95be4a940c2
willp12 <31415008+willp12@users.noreply.github.com>
2024-12-05 21:40:26 -0300

    alteracoes para melhorar performance

--- main.py
+++ main.py
@@ -3,29 +3,28 @@ import folium
 import streamlit as st
 import duckdb
 from streamlit.components.v1 import html
-from streamlit_folium import st_folium
 from folium.plugins import MarkerCluster
 
-#data_path = 'data/CelularesSubtraidos_2024.xlsx'
+# Abrir a conexão com duck
 
 conn = duckdb.connect('data/celulares.duckdb')
 
-df = pd.read_sql('select * from minha_tabela_v2', conn)
-df['LATITUDE'] = pd.to_numeric(df['LATITUDE'], errors='coerce')
-clean_df = df[df['LATITUDE'] < 0]
-clean_df_sp = clean_df[clean_df['NOME_MUNICIPIO'] == 'S.PAULO']
-cluster_data = clean_df_sp[['LATITUDE', 'LONGITUDE']].values.tolist()
+# Capturar o lat long de celulares subtraídos
 
-m = folium.Map(location=(-23.533773, -46.625290), zoom_start=12)
-MarkerCluster(cluster_data).add_to(m)
+df = conn.sql('select * from minha_tabela_v2').df() # transforma a tabela em dataframe
+df['LATITUDE'] = pd.to_numeric(df['LATITUDE'], errors='coerce') # transforma a latitude no tipo numerico
+clean_df = df[df['LATITUDE'] < 0] # considera apenas latitudes menores que 0
+clean_df_sp = clean_df[clean_df['NOME_MUNICIPIO'] == 'S.PAULO'] # considera apenas municipio de são paulo
+cluster_data = clean_df_sp[['LATITUDE', 'LONGITUDE']].values.tolist() # transforma os valores de cluster em lista
+
+# Gerar o objeto mapa de folium e gravar seu HTML
+
+m = folium.Map(location=(-23.533773, -46.625290), zoom_start=12) # gera um mapa com inicio em sao paulo
+MarkerCluster(cluster_data).add_to(m) # adiciona os icones de lat long
 mapa_html = m._repr_html_()
 
 # Renderizar o mapa como HTML e exibir no Streamlit
-st.title("Mapa com Folium no Streamlit")
+
+st.title("Mapa de São Paulo com Celulares Subtraídos")
 st.write("Aqui está o mapa interativo com clusters:")
 html(mapa_html, height=600)
-
-
-
-# Renderizar HTML do mapa
-#st_map = st_folium(m, width=700, height=450)
